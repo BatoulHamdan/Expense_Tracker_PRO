08ce91d8abc0d2fbdba75c96aadd6396e4025559
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-03-13 14:27:10 +0200

    HAndling registration

--- Modules/Users/Controllers/register.js
+++ Modules/Users/Controllers/register.js
@@ -1,6 +1,34 @@
-const register = (req, res) => {
-  res.status(200).json({
-    status: "Hello from register!",
+const mongoose = require("mongoose");
+
+const register = async (req, res) => {
+  const usersModel = mongoose.model("users");
+
+  const { name, email, password, confirm_password, balance } = req.body;
+
+  //Validations
+  if (!name) throw "Name must be provided!";
+  if (!email) throw "Email must be provided!";
+  if (!password) throw "Password must be provided!";
+  if (password.length < 6)
+    throw "Password must be at least 6 characters long!";
+  if (confirm_password !== password)
+    throw "Password and confirm password does not match!";
+
+  const getDuplicateEmail = await usersModel.findOne({
+    email: email,
+  });
+
+  if (getDuplicateEmail) throw "This email already exists!";
+
+  await usersModel.create({
+    name: name,
+    email: email,
+    password: password,
+    balance: balance,
+  });
+
+  res.status(201).json({
+    status: "User registered successfully!",
   });
 };
 
