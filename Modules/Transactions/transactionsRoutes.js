21fa25fcdb4702be59855cd3f99f103dfa64f1b1
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-03-13 17:29:18 +0200

    Implementing getTransactions

--- Modules/Transactions/transactionsRoutes.js
+++ Modules/Transactions/transactionsRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const auth = require("../../Middleware/auth");
 const addIncome = require("./Controllers/addIncome");
 const addExpense = require("./Controllers/addExpense");
+const getTransactions = require("./Controllers/getTransactions");
 
 const transactionRoutes = express.Router();
 
@@ -11,5 +12,6 @@ transactionRoutes.use(auth);
 // Protected routes...
 transactionRoutes.post("/addIncome", addIncome);
 transactionRoutes.post("/addExpense", addExpense);
+transactionRoutes.get("/", getTransactions);
 
 module.exports = transactionRoutes;
