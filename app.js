4b9057e1bc9977569e3d461dfb8d280b7d6991e7
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-03-13 13:30:23 +0200

    Connecting to database

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ require("express-async-errors");
 
 const express = require("express");
 const errorHandler = require("./Handlers/errorHandler");
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 const app = express();
 
-app.use(express.json);
+mongoose
+  .connect(process.env.mongo_connection, {})
+  .then(() => {
+    console.log("Connected to mongoose successfully!");
+  })
+  .catch(() => {
+    console.log("Connection to mongoose failed!");
+  });
 
+app.use(express.json());
 
 //Routes
 
 app.use(errorHandler);
 
 app.listen(8000, () => {
-  console.log("Server started successfully");
+  console.log("Server started successfully!");
 });
