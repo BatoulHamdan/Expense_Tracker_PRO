e45d31779895d54b19c2f8d083633657e863d02d
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-03-13 15:20:37 +0200

    Adding jwt

--- Modules/Users/Controllers/login.js
+++ Modules/Users/Controllers/login.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 
 const login = async (req, res) => {
   const usersModel = mongoose.model("users");
@@ -12,11 +13,20 @@ const login = async (req, res) => {
   if (!getUser) throw "This email does not exist!";
 
   const comparePassword = await bcrypt.compare(password, getUser.password);
-  if(!comparePassword) throw "Email and password do not match!"
+  if (!comparePassword) throw "Email and password do not match!";
+
+  const accessToken = await jsonwebtoken.sign(
+    {
+      _id: getUser._id,
+      name: getUser.name,
+    },
+    process.env.jwt_salt
+  );
 
   res.status(201).json({
     status: "success",
     message: "User logged in successfully!",
+    accessToken: accessToken,
   });
 };
 
--- Modules/Users/usersRoutes.js
+++ Modules/Users/usersRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const register = require("./Controllers/register");
 const login = require("./Controllers/login");
+const userDashboard = require("./Controllers/userDashboard");
 
 const usersRoutes = express.Router();
 
 //Routes
 usersRoutes.post("/register", register);
 usersRoutes.post("/login", login);
+usersRoutes.get("/dashboard", userDashboard);
 
 module.exports = usersRoutes;
