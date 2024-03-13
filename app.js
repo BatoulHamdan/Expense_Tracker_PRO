65fd1eff4735b844b26d708b21566b593090fc7b
BatoulHamdan <batoul.hamdan19@gmail.com>
2024-03-13 13:17:12 +0200

    Preparing environment

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 require("express-async-errors");
 
 const express = require("express");
+const errorHandler = require("./Handlers/errorHandler");
 
 const app = express();
+
+app.use(express.json);
+
+
+//Routes
+
+app.use(errorHandler);
 
 app.listen(8000, () => {
   console.log("Server started successfully");
